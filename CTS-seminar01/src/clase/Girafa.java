03263f9c57cbb74b470359519f7bd252e2616c21
chiritaflorentina <cetelegeny22>
2021-02-27 14:59:19 +0200

    adaugare clase zoo, griafa, sebra

---
+++ CTS-seminar01/src/clase/Girafa.java
@@ -0,0 +1,26 @@
+package clase;
+
+public class Girafa extends Animal{
+
+	private int inaltime;
+	public Girafa(String nume) {
+		super(nume);
+		this.inaltime=48;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getInaltime() {
+		return inaltime;
+	}
+
+	public void setInaltime(int inaltime) {
+		this.inaltime = inaltime;
+	}
+
+	public Girafa(String nume, int inaltime) {
+		super(nume);
+		this.inaltime = inaltime;
+	}
+	
+
+}
---
+++ CTS-seminar01/src/clase/Zoo.java
@@ -0,0 +1,34 @@
+package clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo  {
+ private Zookeeper z;
+ private List<Animal> animals;
+public Zoo(Zookeeper z, List<Animal> animals) {
+	super();
+	this.z = z;
+	this.animals = animals;
+}
+public Zoo() {
+	super();
+	this.z=new Zookeeper("Ion");
+	//List e abstract, trebuie ArrayList la initializare
+	this.animals=new ArrayList<Animal>();	
+
+}
+
+//metoda care permite adaugarea unuui animal in colectia noastra
+public void adaugareAnimal(Animal a)
+{
+	this.animals.add(a);
+}
+
+public void hranesteAnimal() {
+	for(Animal a : this.animals)
+	{
+		this.z.feed(a);
+	}
+}
+}
